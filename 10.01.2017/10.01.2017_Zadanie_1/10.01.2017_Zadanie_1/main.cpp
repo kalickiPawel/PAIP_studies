8def41aec3c81e461a728957e1b54a19afcc90ad
satellite107 <kalicki.paul@icloud.com>
2017-01-10 12:39:39 +0100

    Dodanie komentarzy oraz rozrzenie kodu o mnozenie

--- 10.01.2017/10.01.2017_Zadanie_1/10.01.2017_Zadanie_1/main.cpp
+++ 10.01.2017/10.01.2017_Zadanie_1/10.01.2017_Zadanie_1/main.cpp
@@ -26,17 +26,33 @@ int dodawanie_v2(int a, int b)
     return a+b; // zwrócenie wyniku
 }
 
+//  funkcja odejmowania
+int mnozenie(int c, int d)
+{
+    return c*d; // zwrócenie wyniku
+}
+
 //  funkcja główna
 int main(int argc, const char * argv[]) {
-    //  1. sposób
+    
+    //  1. sposób wyświetlania wartości zwróconych przez funkcję (poprzez ręczne podanie danych)
     cout << dodawanie(5,7);
-    //  2. sposób
+    
+    //  2. sposób wyświetlania wartości zwróconych przez funkcję (poprzez wprowadzenie danych przez użytkownika
     int a, b;
     cin >> a >> b;
     cout << dodawanie(a,b);
-    //  3. sposób
+    
+    //  3. sposób wyświetlania wartości zwróconych przez funkcję (poprzez przypisanie wartości zwróconych do zmiennej)
     int suma;
     suma = dodawanie(a,b);
     cout << suma;
+    
+    //mnozenie
+    
+    int c,d;
+    cin >> c >> d;
+    cout << endl << mnozenie(c,d);
+    
     return 0;
 }
