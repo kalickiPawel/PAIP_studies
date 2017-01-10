2a35eafcd15a8dc58628a43a8e4f93a869829f0f
satellite107 <kalicki.paul@icloud.com>
2017-01-10 12:23:58 +0100

    Tests

---
+++ 10.12.2016/10.01.2017_Zadanie_2/10.01.2017_Zadanie_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  10.01.2017_Zadanie_2
+//
+//  Created by Paweł Kalicki on 10/01/2017.
+//  Copyright © 2017 Paweł Kalicki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
