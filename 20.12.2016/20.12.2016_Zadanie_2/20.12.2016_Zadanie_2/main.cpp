8e21f0f52dada1d32bd605be9f02c138d4e3fe0d
satellite107 <kalicki.paul@icloud.com>
2017-01-22 14:25:41 +0100

    Poprawki w switch

--- 20.12.2016/20.12.2016_Zadanie_2/20.12.2016_Zadanie_2/main.cpp
+++ 20.12.2016/20.12.2016_Zadanie_2/20.12.2016_Zadanie_2/main.cpp
@@ -15,57 +15,61 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    int x;
-    char powrot;
+    
+    int zmienna;
     
     cout << "Tablica jednowymiarowa wybierz: 0" << endl;
     cout << "Tablica dwuwymiarowa wybierz: 1" << endl;
-    cin >> x;
+    cin >> zmienna;
     
-    do{
-        switch(x)
+    do
+    {
+        switch(zmienna)
         {
+            case 0:
             
-        case 0:
+                int rozmiar = 0;
+                int wartosc = 0;
+    
+                cout << "Podaj rozmiar tablicy: ";
+                cin >> rozmiar;
+    
+                int *p = new int[rozmiar]; //   alokuję pamięć na tablicę w sposób dynamiczny
+    
+                cout << "Stworzono tablice o " << rozmiar << " elementach" << endl;
+    
+                for(int i=0; i<rozmiar; i++)
+                {
+                    cout << "Podaj wartosc " << i << " elementu: ";
+                    cin >> wartosc;
+                    p[i] = wartosc;
+                }
+    
+                cout << "Elementy w tablicy to:" << endl;
+    
+                for(int i=0; i<rozmiar; i++)
+                {
+                    cout << p[i] << " ";
+                }
+    
+                cout << endl;
+    
+                delete[] p; //  usuwanie pamięci tablicy
             
-            int rozmiar = 0;
-            int wartosc = 0;
-    
-            cout << "Podaj rozmiar tablicy: ";
-            cin >> rozmiar;
-    
-            int *p = new int[rozmiar]; //   alokuję pamięć na tablicę w sposób dynamiczny
-    
-            cout << "Stworzono tablice o " << rozmiar << " elementach" << endl;
-    
-            for(int i=0; i<rozmiar; i++)
-            {
-                cout << "Podaj wartosc " << i << " elementu: ";
-                cin >> wartosc;
-                p[i] = wartosc;
-            }
-    
-            cout << "Elementy w tablicy to:" << endl;
-    
-            for(int i=0; i<rozmiar; i++)
-            {
-                cout << p[i] << " ";
-            }
-    
-            cout << endl;
-    
-            delete[] p; //  usuwanie pamięci tablicy
-            
-            break;
-            
-        case 1:
-                cout << "Sektor 1" << endl;;
                 break;
-        default:
-            cout << "Nie wybrałeś tablicy wpisz \"x\" aby powrocic" << endl;
-            cin >> powrot;
-            break;
+                
+            case 1:
+                
+                cout << "Sektor 1" << endl;
+                
+                break;
+                
+            default:
+                
+                cout << "Nie wybrałeś tablicy!" << endl;
+                break;
         }
-    }while(powrot!=x);
+    }while(zmienna != 1 || zmienna != 0);
+    
     return 0;
 }
